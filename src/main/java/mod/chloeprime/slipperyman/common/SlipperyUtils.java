3fa968f8fb95bf22db90f4fc887a12b7d7f225c4
ChloePrime <chloe_von_einzbern@qq.com>
2024-02-04 16:16:56 +0800

    蹲下时不增加 step height 并无视 step height 属性 版本1.0.5

--- src/main/java/mod/chloeprime/slipperyman/common/SlipperyUtils.java
+++ src/main/java/mod/chloeprime/slipperyman/common/SlipperyUtils.java
@@ -9,6 +9,12 @@ import java.util.function.Consumer;
 public class SlipperyUtils {
     public static final Vec3 UP = new Vec3(0, 1, 0);
 
+    /**
+     * instanceof，但是避免在 mixin 类中直接使用 instanceof 并进行后续操作会导致 idea 产生 {@value "ConstantValue"} 警告的缺陷。
+     * <p>
+     * @param entity 要判断是否为 {@link Player} 的实体
+     * @return entity 是否为 {@link Player}
+     */
     public static boolean isPlayer(Entity entity) {
         return entity instanceof Player;
     }
--- src/main/java/mod/chloeprime/slipperyman/mixin/MixinPlayer.java
+++ src/main/java/mod/chloeprime/slipperyman/mixin/MixinPlayer.java
@@ -1,5 +1,6 @@
 package mod.chloeprime.slipperyman.mixin;
 
+import mod.chloeprime.slipperyman.common.SlipperyUtils;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -11,12 +12,19 @@ public abstract class MixinPlayer extends LivingEntity {
     @Override
     @SuppressWarnings("deprecation")
     public float getStepHeight() {
+        if (!SlipperyUtils.isPlayer(this)) {
+            return super.getStepHeight();
+        }
+        // 蹲下时会无视其他模组的 stepHeight
+        if (isShiftKeyDown()) {
+            return maxUpStep;
+        }
         var recordedStepHeight = maxUpStep;
         if (recordedStepHeight >= 1) {
             return super.getStepHeight();
         }
         try {
-            maxUpStep = 1;
+            maxUpStep = isShiftKeyDown() ? 0.6F : 1F;
             return super.getStepHeight();
         } finally {
             maxUpStep = recordedStepHeight;
